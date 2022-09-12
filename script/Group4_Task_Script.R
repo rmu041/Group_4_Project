c397795ab3f0be106c3fa7e6d57008cec316af84
Rammah Elnour <r.k.elnour@gmail.com>
2022-09-12 12:09:01 +0200

    Updating main branch with script and environment data

--- script/Group4_Task_Script.R
+++ script/Group4_Task_Script.R
@@ -2,3 +2,14 @@
 
 ## Creating a script for group 4 task ##
 
+## Loading necessary packages ##
+library(tidyverse)
+
+## Loading data to environment using read.csv ##
+
+Group_4_data <- read.csv(here("data", "exam_nontidy - kopia.txt"))
+view(Group_4_data)
+
+## Loading data to environment using read.csv ##
+
+Group_4_data <- read_delim(here("data", "exam_nontidy - kopia.txt"), delim = "\t")
