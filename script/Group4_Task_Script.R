280eb053db99e36dd04492e04ff716f427b50fec
Rammah Elnour <r.k.elnour@gmail.com>
2022-09-13 11:08:46 +0200

    Updating script with pivot function to pivot various features/measurements in one column to wider data set

--- script/Group4_Task_Script.R
+++ script/Group4_Task_Script.R
@@ -7,23 +7,18 @@ library(tidyverse)
 
 ## Loading data to environment using read.csv ##
 
-Group_4_data <- read.csv(here("data", "exam_nontidy - kopia.txt"))
-view(Group_4_data)
-
-## Loading data to environment using read.csv ##
-
 Group_4_data <- read_delim(here("data", "exam_nontidy - kopia.txt"), delim = "\t")
 
 #separate pregnancy and age
-Exam_data_1 <- 
-Exam_data_1 %>%
+Group_4_data <- 
+  Group_4_data %>%
   separate(col = `pregnancy_num-age`,
            into = c("pregnancy_num", "age"),
            sep = "-")
 
 #separate hospital and subject
-Exam_data_1 <- 
-Exam_data_1 %>%
+Group_4_data <- 
+  Group_4_data %>%
   separate(col = subject,
            into = c("hospital", "subject"),
            sep = "-")
