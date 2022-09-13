b42f7ef182587f24a7b6f6a7ba6278f85c998630
ttsuib <112323014+ttsuib@users.noreply.github.com>
2022-09-13 10:50:46 +0200

    Update Group4_Task_Script.R
    Separating pregnancy/age and hospital/subject.

--- script/Group4_Task_Script.R
+++ script/Group4_Task_Script.R
@@ -13,3 +13,17 @@ view(Group_4_data)
 ## Loading data to environment using read.csv ##
 
 Group_4_data <- read_delim(here("data", "exam_nontidy - kopia.txt"), delim = "\t")
+
+#separate pregnancy and age
+Exam_data_1 <- 
+Exam_data_1 %>%
+  separate(col = `pregnancy_num-age`,
+           into = c("pregnancy_num", "age"),
+           sep = "-")
+
+#separate hospital and subject
+Exam_data_1 <- 
+Exam_data_1 %>%
+  separate(col = subject,
+           into = c("hospital", "subject"),
+           sep = "-")
