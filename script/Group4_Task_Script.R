3d1c4b964f19f039ea8b544ca69859f2312f335d
Rammah Elnour <r.k.elnour@gmail.com>
2022-09-13 11:11:51 +0200

    Pivoting columns with values from various measurements to expand values from various features/measurements from one column

--- script/Group4_Task_Script.R
+++ script/Group4_Task_Script.R
@@ -1,16 +1,5 @@
 ## Group 4 task ##
 
-## Creating a script for group 4 task ##
-
-## Loading necessary packages ##
-library(here)
-library(tidyverse)
-
-## Loading data to environment using read.delim ##
-
-Group_4_data <- read_delim(here("data", "exam_nontidy - kopia.txt"), delim = "\t")
-view(Group_4_data)
-
 ## Pivoting columns with values from various measurements----
 Group_4_data <- Group_4_data %>% 
   pivot_wider(names_from = "measured variable",
