e038b87569f9086c0d935a33c0d4419389821eb2
Rammah Elnour <r.k.elnour@gmail.com>
2022-09-13 11:22:14 +0200

    ...

--- script/Group4_Task_Script.R
+++ script/Group4_Task_Script.R
@@ -1,4 +1,4 @@
-## Pivoting columns with values from various measurements----
+## Pivoting columns with values from various measurements
 Group_4_data <- Group_4_data %>% 
   pivot_wider(names_from = "measured variable",
               values_from = "value")
