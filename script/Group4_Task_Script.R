b144a02db61c0bf9a186d2460feb4ba17938b81c
Rammah Elnour <r.k.elnour@gmail.com>
2022-09-13 10:55:01 +0200

    Updating day 5 script with pivot function to expand various features/measurements from one column into wider dataframe

--- script/Group4_Task_Script.R
+++ script/Group4_Task_Script.R
@@ -3,13 +3,15 @@
 ## Creating a script for group 4 task ##
 
 ## Loading necessary packages ##
+library(here)
 library(tidyverse)
 
-## Loading data to environment using read.csv ##
-
-Group_4_data <- read.csv(here("data", "exam_nontidy - kopia.txt"))
-view(Group_4_data)
-
-## Loading data to environment using read.csv ##
+## Loading data to environment using read.delim ##
 
 Group_4_data <- read_delim(here("data", "exam_nontidy - kopia.txt"), delim = "\t")
+view(Group_4_data)
+
+## Pivoting columns with values from various measurements----
+Group_4_data <- Group_4_data %>% 
+  pivot_wider(names_from = "measured variable",
+              values_from = "value")
