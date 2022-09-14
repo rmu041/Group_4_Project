54a0d610e9b83fcf1092de9cb7f620a4855d3d51
Rammah Elnour <r.k.elnour@gmail.com>
2022-09-14 12:23:05 +0200

    Cleaned up script and added contributions from group members

--- script/Group4_Task_Script.R
+++ script/Group4_Task_Script.R
@@ -6,33 +6,50 @@
 library(tidyverse)
 library(here)
 
-## Loading data to environment using read.csv ##
-Group_4_data <- read.csv(here("DATA_copy", "exam_nontidy_Johannacopy.txt"))
-view(Group_4_data)
+#Day 5----
 
-##Separate columns
-Group_4_data <- read_delim(here("DATA_copy", "exam_nontidy_Johannacopy.txt"), delim = "\t")
+##Loading data----
+Group_4_data <- read_delim(here("data", "exam_nontidy - copy.txt"), delim = "\t")
 
-#separate pregnancy and age
+##Separate columns----
 Group_4_data <- 
   Group_4_data %>%
   separate(col = `pregnancy_num-age`,
            into = c("pregnancy_num", "age"),
            sep = "-")
 
-# removing duplicated columns
-Group_4_data <-
-  Group_4_data %>%
-    distinct()
-
-#separate hospital and subject
+##separate hospital and subject
 Group_4_data <- 
   Group_4_data %>%
   separate(col = subject,
            into = c("hospital", "subject"),
            sep = "-")
 
-##Change column names that contain spaces or start with numbers (or characters?)
+## Removing duplicates----
+Group_4_data <-
+  Group_4_data %>%
+  distinct()
+
+## Pivoting columns with values from various measurements----
+Group_4_data <- Group_4_data %>% 
+  pivot_wider(names_from = "measured_variable",
+              values_from = "value")
+
+#Day 6----
+
+## Remove  columns from your dataframe: choleste, sibling ----
+Group_4_data <- 
+  Group_4_data %>% 
+  select(-c(sibling, choleste))
+
+## Variable type changes ----
+Group_4_data <- 
+  Group_4_data %>%
+  mutate(pregnancy_num = as.numeric(pregnancy_num),
+         age = as.numeric(age),
+         id = as.numeric(id))
+
+##Change column names that contain spaces or start with numbers (or characters?)----
 Group_4_data <- 
 Group_4_data %>% 
   rename(id = `subject`,
@@ -42,33 +59,42 @@ Group_4_data %>%
          value = `.value`
          )
 
-##Set the order of columns as required (id-hospital-age, the rest)
-Group_4_data <- 
+## Create set of columns for: glucose_mg_dl>120, insulin in units pmol/L ----
+## diabetes_5y as 0/1, multiplication of age and pregnancy_num
+Group_4_data <-
   Group_4_data %>% 
-  select(id, hospital, age, everything()) ##select() picks variables based on their names, everything() selects all variables
+  mutate(glucose_mg_dl_classifier = if_else(glucose_mg_dl >= 120, "High", "Low")) %>% 
+  mutate(insulin_microiu_pmol_l = insulin_microiu_ml*6.945) %>% 
+  mutate(diabetes_5y_classifier = if_else(diabetes_5y == "pos", "1","0")) %>% 
+  mutate(multiply_age_pregnancy = age*pregnancy_num) %>% 
+  select(id,hospital,age,everything()) %>% 
+  arrange(id)
 
-##Arrange ID column of your dataset in order of increasing number
-Group_4_data <- 
-  Group_4_data %>% 
-  mutate_at(c("id"), as.numeric) #change characters to numeric otherwise the order is 1 10 11 2 21 22
+##Read,join additional dataset to your main dataset----
+Joining_data <- read_delim(here("data", "exam_joindata_copy_1.txt"), delim = "\t")
 
-Group_4_data <- 
-  Group_4_data %>% 
-  arrange(Group_4_data, id)
-
-##Join additional data
-Group_4_additional <- read.csv(here("DATA_copy", "exam_joindata_Johannacopy.txt"))
-view(Group_4_additional)
-
-##Separate additional data columns
-Group_4_additional <- read_delim(here("DATA_copy", "exam_joindata_Johannacopy.txt"), delim = "\t")
-
-#Make additional id column numeric
-Group_4_additional <-
-  Group_4_additional %>%
-  mutate(id = as.numeric(id))
-
-##Join additional dataset to main dataset
-Complete_data <-
+##Joining datasets----
+Group_4_joined_data <-
   Group_4_data %>%
-  left_join(Group_4_additional, by = "id")
+  left_join(Joining_data, by = "id")
+
+
+##Explore data again after adjustments ----
+summary(Group_4_data)
+glimpse(Group_4_data)
+skimr::skim(Group_4_data)
+naniar::gg_miss_var(Group_4_data)
+naniar::gg_miss_var(Group_4_data, facet = five_year_diabetes_classifier)
+
+##Comment on missing data?----
+#PROVIDE ANSWER HERE
+
+##Stratify data by categorical column: min,max,mean,sd----
+Group_4_joined_data %>% 
+  group_by(glucose_mg_dl_classifier) %>% 
+  summarise(max(bmi, na.rm = T), min(bmi, na.rm = T), mean(bmi, na.rm = T), sd(bmi, na.rm = T))
+
+
+
+#Day 7----
+
